f217240d1cb57a6738508c1b003943e38e899814
roubachof <jm@sharpnado.com>
2020-03-06 10:58:46 +0100

    Initial Commit

---
+++ NukeProxy/NukeProxy.h
@@ -0,0 +1,19 @@
+//
+//  NukeProxy.h
+//  NukeProxy
+//
+//  Created by Jean-Marie Alfonsi on 06/03/2020.
+//  Copyright © 2020 Jean-Marie Alfonsi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NukeProxy.
+FOUNDATION_EXPORT double NukeProxyVersionNumber;
+
+//! Project version string for NukeProxy.
+FOUNDATION_EXPORT const unsigned char NukeProxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NukeProxy/PublicHeader.h>
+
+
