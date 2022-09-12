24c094ba3b36151432ea82b3d3afe6047f59067f
Tomasz Cielecki <tci@trackman.com>
2022-09-12 11:28:37 +0200

    Add net6 projects + solution filters

---
+++ Xamarin.Nuke/Xamarin.Nuke.NET6/ApiDefinition.cs
@@ -0,0 +1,90 @@
+using System;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace Xamarin.Nuke
+{
+	// @interface DataLoader : NSObject
+	[BaseType (typeof(NSObject))]
+	interface DataLoader
+	{
+		// @property (readonly, nonatomic, strong, class) DataLoader * _Nonnull shared;
+		[Static]
+		[Export ("shared", ArgumentSemantic.Strong)]
+		DataLoader Shared { get; }
+
+		// -(void)removeAllCachedResponses;
+		[Export ("removeAllCachedResponses")]
+		void RemoveAllCachedResponses ();
+	}
+
+	// @interface ImageCache : NSObject
+	[BaseType (typeof(NSObject))]
+	interface ImageCache
+	{
+		// @property (readonly, nonatomic, strong, class) ImageCache * _Nonnull shared;
+		[Static]
+		[Export ("shared", ArgumentSemantic.Strong)]
+		ImageCache Shared { get; }
+
+		// -(void)removeAll;
+		[Export ("removeAll")]
+		void RemoveAll ();
+	}
+
+	// @interface ImagePipeline : NSObject
+	[BaseType (typeof(NSObject))]
+	interface ImagePipeline
+	{
+		// @property (readonly, nonatomic, strong, class) ImagePipeline * _Nonnull shared;
+		[Static]
+		[Export ("shared", ArgumentSemantic.Strong)]
+		ImagePipeline Shared { get; }
+
+		// -(void)loadImageWithUrl:(NSURL * _Nonnull)url onCompleted:(void (^ _Nonnull)(UIImage * _Nullable, NSString * _Nonnull))onCompleted;
+		[Export ("loadImageWithUrl:onCompleted:")]
+		void LoadImageWithUrl (NSUrl url, Action<UIImage, NSString> onCompleted);
+
+		// -(void)loadImageWithUrl:(NSURL * _Nonnull)url placeholder:(UIImage * _Nullable)placeholder errorImage:(UIImage * _Nullable)errorImage into:(UIImageView * _Nonnull)into;
+		[Export ("loadImageWithUrl:placeholder:errorImage:into:")]
+		void LoadImageWithUrl (NSUrl url, [NullAllowed] UIImage placeholder, [NullAllowed] UIImage errorImage, UIImageView into);
+
+		// -(void)loadImageWithUrl:(NSURL * _Nonnull)url imageIdKey:(NSString * _Nonnull)imageIdKey placeholder:(UIImage * _Nullable)placeholder errorImage:(UIImage * _Nullable)errorImage into:(UIImageView * _Nonnull)into;
+		[Export ("loadImageWithUrl:imageIdKey:placeholder:errorImage:into:")]
+		void LoadImageWithUrl (NSUrl url, string imageIdKey, [NullAllowed] UIImage placeholder, [NullAllowed] UIImage errorImage, UIImageView into);
+
+		// -(void)loadDataWithUrl:(NSURL * _Nonnull)url onCompleted:(void (^ _Nonnull)(NSData * _Nullable, NSURLResponse * _Nullable))onCompleted;
+		[Export ("loadDataWithUrl:onCompleted:")]
+		void LoadDataWithUrl (NSUrl url, Action<NSData, NSUrlResponse> onCompleted);
+
+		// -(void)loadDataWithUrl:(NSURL * _Nonnull)url imageIdKey:(NSString * _Nullable)imageIdKey reloadIgnoringCachedData:(BOOL)reloadIgnoringCachedData onCompleted:(void (^ _Nonnull)(NSData * _Nullable, NSURLResponse * _Nullable))onCompleted;
+		[Export ("loadDataWithUrl:imageIdKey:reloadIgnoringCachedData:onCompleted:")]
+		void LoadDataWithUrl (NSUrl url, [NullAllowed] string imageIdKey, bool reloadIgnoringCachedData, Action<NSData, NSUrlResponse> onCompleted);
+	}
+
+	// @interface Prefetcher : NSObject
+	[BaseType (typeof(NSObject))]
+	interface Prefetcher
+	{
+		// -(void)startPrefetchingWith:(NSArray<NSURL *> * _Nonnull)with;
+		[Export ("startPrefetchingWith:")]
+		void StartPrefetchingWith (NSUrl[] with);
+
+		// -(void)stopPrefetchingWith:(NSArray<NSURL *> * _Nonnull)with;
+		[Export ("stopPrefetchingWith:")]
+		void StopPrefetchingWith (NSUrl[] with);
+
+		// -(void)stopPrefetching;
+		[Export ("stopPrefetching")]
+		void StopPrefetching ();
+
+		// -(void)pause;
+		[Export ("pause")]
+		void Pause ();
+
+		// -(void)unPause;
+		[Export ("unPause")]
+		void UnPause ();
+	}
+}
---
+++ Xamarin.Nuke/Xamarin.Nuke.SampleNET6/ViewController.cs
@@ -0,0 +1,118 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using SkiaSharp.Views.iOS;
+using SkiaSharp;
+
+namespace Xamarin.Nuke.Sample
+{
+    public partial class ViewController : UIViewController
+    {
+        private SKBitmap _bitmap;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            var image = new UIImageView
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false
+            };
+            
+            Add(image);
+
+            ImagePipeline.Shared.LoadImageWithUrl(
+                new NSUrl("https://placekitten.com/g/300/300"),
+                (img, url) => image.Image = img);
+
+            UIButton button = AddButton("Placeholder");
+
+            button.TouchUpInside += (s, e) =>
+            {
+                ImageCache.Shared.RemoveAll();
+
+                ImagePipeline.Shared.LoadImageWithUrl(
+                    new NSUrl("https://placekitten.com/g/1000/1000"),
+                    UIImage.FromBundle("Placeholder"),
+                    null,
+                    image);
+            };
+
+            var skiaView = new SKCanvasView
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false
+            };
+            skiaView.PaintSurface += OnPaintSurface;
+            Add(skiaView);
+
+            UIButton button2 = AddButton("Streamed image");
+
+            button2.TouchUpInside += (s, e) =>
+            {
+                ImagePipeline.Shared.LoadDataWithUrl(new NSUrl("https://placekitten.com/g/1000/1000"), (data, response) =>
+                {
+                    using var dataStream = data.AsStream();
+                    _bitmap = SKBitmap.Decode(dataStream);
+
+                    skiaView.LayoutSubviews();
+                });
+            };
+
+            AddConstraints(image, button, button2, skiaView);
+        }
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            if (_bitmap == null)
+                return;
+
+            e.Surface.Canvas.DrawBitmap(_bitmap, SKPoint.Empty);
+        }
+
+        private void AddConstraints(UIImageView image, UIButton button, UIButton button2, SKCanvasView skiaView)
+        {
+            image.WidthAnchor.ConstraintEqualTo(View.WidthAnchor).Active = true;
+            image.HeightAnchor.ConstraintEqualTo(View.WidthAnchor).Active = true;
+            image.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+            image.TopAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.TopAnchor).Active = true;
+
+            button.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor, 16).Active = true;
+            button.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor, -16).Active = true;
+            button.BottomAnchor.ConstraintEqualTo(View.SafeAreaLayoutGuide.BottomAnchor, -20).Active = true;
+
+            button2.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor, 16).Active = true;
+            button2.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor, -16).Active = true;
+            button2.BottomAnchor.ConstraintEqualTo(button.TopAnchor, -20).Active = true;
+
+            skiaView.TopAnchor.ConstraintEqualTo(image.BottomAnchor, 10).Active = true;
+            skiaView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor, 16).Active = true;
+            skiaView.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor, -16).Active = true;
+            skiaView.BottomAnchor.ConstraintEqualTo(button2.TopAnchor, -16).Active = true;
+        }
+
+        private UIButton AddButton(string title)
+        {
+            var button = new UIButton
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                BackgroundColor = UIColor.Orange
+            };
+            button.SetTitleColor(UIColor.Black, UIControlState.Normal);
+            button.SetTitle(title, UIControlState.Normal);
+
+            Add(button);
+            return button;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
