76456015b9f833d92ada762ee5d8b51072b49270
Tomasz Cielecki <249719+Cheesebaron@users.noreply.github.com>
2025-02-20 12:42:23 +0100

    Update build script with same hack as in workflow

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 echo "Update carthage deps"
 sh carthage.sh update --use-xcframeworks --platform iOS --log-path artifacts/carthage.log
 
+echo 'exit 0' > Carthage/Checkouts/Nuke/Scripts/lint.sh
+echo 'exit 0' > Carthage/Checkouts/Nuke/Scripts/validate.sh
+
+sh carthage.sh build --use-xcframeworks --platform iOS --log-path artifacts/carthage.log
+
 echo "xcode build"
 xcodebuild archive -sdk iphoneos -project NukeProxy.xcodeproj -scheme NukeProxy -configuration Release -archivePath Output/Output-iphoneos SKIP_INSTALL=NO
 xcodebuild archive -sdk iphonesimulator -project NukeProxy.xcodeproj -scheme NukeProxy -configuration Release -archivePath Output/Output-iphonesimulator SKIP_INSTALL=NO
